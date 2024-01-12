6a179982cd15e0beeadb6036666c4443dea5be50
Samuseu <s67.freelance@gmail.com>
2024-01-12 13:30:12 +0300

    add attaches-screenshot,log

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -1,10 +1,15 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import demoqa.data.TestData;
+import demoqa.helpers.Attach;
 import demoqa.pages.RegistrationPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -28,5 +33,16 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        //Attach.video();
     }
 }
