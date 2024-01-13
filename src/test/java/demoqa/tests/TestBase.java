b78379e6afc3420b7c7b5e8c174735f9f4f46ca6
Vlad <s67.freelance@gmail.com>
2024-01-13 21:50:38 +0300

    update remote

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -1,7 +1,6 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import demoqa.data.TestData;
 import demoqa.helpers.Attach;
@@ -10,15 +9,8 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class TestBase {
@@ -29,29 +21,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() throws MalformedURLException {
         Configuration.baseUrl = "https://demoqa.com";
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("browserVersion", "120.0");
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
-            put("name", "Test badge...");
-
-            /* How to set session timeout */
-            put("sessionTimeout", "15m");
-
-            /* How to set timezone */
-            put("env", new ArrayList<String>() {{
-                add("TZ=UTC");
-            }});
-
-            /* How to add "trash" button */
-            put("labels", new HashMap<String, Object>() {{
-                put("manual", "true");
-            }});
-
-            /* How to enable video recording */
-            put("enableVideo", true);
-        }});
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:8888/wd/hub"), options);
+        Configuration.remote = "http://localhost:4444/wd/hub";
     }
 
     @BeforeEach
