e090e6b58e47143fed1dd4028ef957130108c26f
Vlad <s67.freelance@gmail.com>
2024-01-15 00:24:34 +0300

    update add Configuration.remote = System.getProperty("remote_selenoid","http://selenoid:4444/wd/hub");                    Configuration.browser=System.getProperty("browser","chrome");                    Configuration.browserSize=System.getProperty("browser_size","1920x1080");

--- src/test/java/demoqa/helpers/Attach.java
+++ src/test/java/demoqa/helpers/Attach.java
@@ -44,7 +44,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+//        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String videoUrl = System.getProperty("attach_video","https://selenoid.autotests.cloud/video/") + sessionId() + ".mp4";
 //        System.out.println(sessionId());
         try {
             return new URL(videoUrl);
