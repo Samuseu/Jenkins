d3e2c97ea27ef28cc04d3e1e822a4baec3b16704
Vlad <s67.freelance@gmail.com>
2024-01-15 00:39:17 +0300

    try update

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -9,14 +9,9 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 
@@ -32,36 +27,14 @@ public class TestBase {
         Configuration.remote = System.getProperty("remote_selenoid","http://selenoid:4444/wd/hub");
         Configuration.browser=System.getProperty("browser","chrome");
         Configuration.browserSize=System.getProperty("browser_size","1920x1080");
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//        Configuration.browserCapabilities = capabilities;
-        ChromeOptions options = new ChromeOptions();
-        options.setCapability("browserVersion", "120.0");
-        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
-            put("name", "Test badge...");
 
-            /* How to set session timeout */
-            put("sessionTimeout", "15m");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
-            /* How to set timezone */
-            put("env", new ArrayList<String>() {{
-                add("TZ=UTC");
-            }});
-
-            /* How to add "trash" button */
-            put("labels", new HashMap<String, Object>() {{
-                put("manual", "true");
-            }});
-
-            /* How to enable video recording */
-            put("enableVideo", true);
-        }});
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://selenoid:4444/wd/hub"), options);
     }
 
     @BeforeEach
