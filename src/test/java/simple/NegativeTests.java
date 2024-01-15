e61d55fb71866db940415734bb0b9a164d67dfb3
Vlad <s67.freelance@gmail.com>
2024-01-15 16:04:56 +0300

    try update

--- src/test/java/simple/NegativeTests.java
+++ src/test/java/simple/NegativeTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@Tag("negative")
+@Tag("simple")
 public class NegativeTests {
     @Test
     void test1(){
