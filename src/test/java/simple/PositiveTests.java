441232009332e10b9df96d3bd65e5db031fa12fc
Vlad <s67.freelance@gmail.com>
2024-01-15 16:01:28 +0300

    try update

--- src/test/java/simple/PositiveTests.java
+++ src/test/java/simple/PositiveTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-@Tag("remote_new")
+@Tag("simple")
 public class PositiveTests {
     @Test
     void test1(){
